29d32d71b23144d710d7e3de8d6b8c36fd4e3ce9
jessicasyu <jessicayu@u.northwestern.edu>
2022-10-14 13:15:53 -0400

    Add tasks for loading and saving text files

--- src/io_collection/load/__init__.py
+++ src/io_collection/load/__init__.py
@@ -1,2 +1,3 @@
 from .task_load_buffer import load_buffer
+from .task_load_text import load_text
 from .task_load_dataframe import load_dataframe
--- src/io_collection/save/__init__.py
+++ src/io_collection/save/__init__.py
@@ -1,2 +1,3 @@
 from .task_save_buffer import save_buffer
+from .task_save_text import save_text
 from .task_save_dataframe import save_dataframe
