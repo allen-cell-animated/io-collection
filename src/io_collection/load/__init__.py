c9eba0558873279e7388ae8d404b2bc9d7a70b76
jessicasyu <jessicayu@u.northwestern.edu>
2022-10-19 14:24:21 -0400

    Add task for loading tar

--- src/io_collection/load/__init__.py
+++ src/io_collection/load/__init__.py
@@ -1,3 +1,4 @@
-from .task_load_buffer import load_buffer
-from .task_load_text import load_text
-from .task_load_dataframe import load_dataframe
+from .load_buffer import load_buffer
+from .load_dataframe import load_dataframe
+from .load_tar import load_tar
+from .load_text import load_text
