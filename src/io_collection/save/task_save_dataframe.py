f72e8340c314f7519720f56a7d2671efe8a1a3a1
jessicasyu <jessicayu@u.northwestern.edu>
2022-10-20 18:07:22 -0400

    Update dataframe tasks to pass kwargs

--- src/io_collection/save/task_save_dataframe.py
+++ src/io_collection/save/task_save_dataframe.py
@@ -1,5 +1,6 @@
 import io
 import os
+from typing import Any
 
 from prefect import task
 import pandas as pd
@@ -8,25 +9,23 @@ from io_collection.save.task_save_buffer import save_buffer_to_s3
 
 
 @task
-def save_dataframe(location: str, key: str, dataframe: pd.DataFrame, index: bool = True) -> None:
+def save_dataframe(location: str, key: str, dataframe: pd.DataFrame, **kwargs: Any) -> None:
     if location[:5] == "s3://":
-        save_dataframe_to_s3(location[5:], key, dataframe, index)
+        save_dataframe_to_s3(location[5:], key, dataframe, **kwargs)
     else:
-        save_dataframe_to_fs(location, key, dataframe, index)
+        save_dataframe_to_fs(location, key, dataframe, **kwargs)
 
 
-def save_dataframe_to_fs(path: str, key: str, dataframe: pd.DataFrame, index: bool = True) -> None:
+def save_dataframe_to_fs(path: str, key: str, dataframe: pd.DataFrame, **kwargs: Any) -> None:
     full_path = os.path.join(path, key)
     os.makedirs(os.path.split(full_path)[0], exist_ok=True)
-    dataframe.to_csv(full_path, index=index)
+    dataframe.to_csv(full_path, **kwargs)
 
 
-def save_dataframe_to_s3(
-    bucket: str, key: str, dataframe: pd.DataFrame, index: bool = True
-) -> None:
+def save_dataframe_to_s3(bucket: str, key: str, dataframe: pd.DataFrame, **kwargs: Any) -> None:
     """
     Saves dataframe to bucket with given key.
     """
     with io.BytesIO() as buffer:
-        dataframe.to_csv(buffer, index=index)
+        dataframe.to_csv(buffer, **kwargs)
         save_buffer_to_s3(bucket, key, buffer)
