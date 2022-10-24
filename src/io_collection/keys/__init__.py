f367035c0b98176857730523988cee73b37f6317
Jessica S. Yu <jesyu@uw.edu>
2022-10-24 18:13:00 -0400

    Add key tasks (#2)
    * Add task for checking if key exists
    
    * Add task for making keys
    
    * Rename files to remove task prefix
    
    * Add task for getting keys

---
+++ src/io_collection/keys/__init__.py
@@ -0,0 +1,3 @@
+from .check_key_exists import check_key_exists
+from .make_key import make_key
+from .get_keys import get_keys
---
+++ src/io_collection/keys/check_key_exists.py
@@ -0,0 +1,23 @@
+import os
+
+import boto3
+from prefect import task
+
+
+@task
+def check_key_exists(location: str, key: str) -> bool:
+    if location[:5] == "s3://":
+        return check_key_exists_s3(location[5:], key)
+    else:
+        return check_key_exists_fs(location, key)
+
+
+def check_key_exists_fs(path: str, key: str) -> bool:
+    full_path = os.path.join(path, key)
+    return os.path.isfile(full_path)
+
+
+def check_key_exists_s3(bucket: str, key: str) -> bool:
+    s3_client = boto3.client("s3")
+    result = s3_client.list_objects(Bucket=bucket, Prefix=key)
+    return "Contents" in result
