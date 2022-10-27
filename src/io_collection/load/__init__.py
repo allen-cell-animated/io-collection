4a367c8166d6969f4380c7b0e80a10850478a445
jessicasyu <jesyu@uw.edu>
2022-10-27 11:22:10 -0400

    Add load image task

--- src/io_collection/load/__init__.py
+++ src/io_collection/load/__init__.py
@@ -1,4 +1,5 @@
 from .load_buffer import load_buffer
 from .load_dataframe import load_dataframe
+from .load_image import load_image
 from .load_tar import load_tar
 from .load_text import load_text
---
+++ src/io_collection/load/load_image.py
@@ -0,0 +1,22 @@
+import os
+
+from prefect import task
+from aicsimageio import AICSImage
+
+
+@task
+def load_image(location: str, key: str) -> AICSImage:
+    if location[:5] == "s3://":
+        return load_image_from_s3(location[5:], key)
+    else:
+        return load_image_from_fs(location, key)
+
+
+def load_image_from_fs(path: str, key: str) -> AICSImage:
+    full_path = os.path.join(path, key)
+    return AICSImage(full_path)
+
+
+def load_image_from_s3(bucket: str, key: str) -> AICSImage:
+    full_key = f"s3://{bucket}/{key}"
+    return AICSImage(full_key)
