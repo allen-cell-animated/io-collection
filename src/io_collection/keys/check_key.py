26af589953e7b1a327a593f967ec30109012aace
Jessica S. Yu <jesyu@uw.edu>
2022-10-27 11:56:03 -0400

    Add change key task (#7)
    * Add change key task
    
    * Rename check key exists task
    
    * Rename get key internal methods

--- src/io_collection/keys/check_key.py
+++ src/io_collection/keys/check_key.py
@@ -5,19 +5,19 @@ from prefect import task
 
 
 @task
-def check_key_exists(location: str, key: str) -> bool:
+def check_key(location: str, key: str) -> bool:
     if location[:5] == "s3://":
-        return check_key_exists_s3(location[5:], key)
+        return check_key_on_s3(location[5:], key)
     else:
-        return check_key_exists_fs(location, key)
+        return check_key_on_fs(location, key)
 
 
-def check_key_exists_fs(path: str, key: str) -> bool:
+def check_key_on_fs(path: str, key: str) -> bool:
     full_path = os.path.join(path, key)
     return os.path.isfile(full_path)
 
 
-def check_key_exists_s3(bucket: str, key: str) -> bool:
+def check_key_on_s3(bucket: str, key: str) -> bool:
     s3_client = boto3.client("s3")
     result = s3_client.list_objects(Bucket=bucket, Prefix=key)
     return "Contents" in result
