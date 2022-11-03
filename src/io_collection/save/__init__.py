420fc0afb226cd6efe6c8f1a8fa7fd0a01f9cbcf
Jessica S. Yu <jesyu@uw.edu>
2022-11-02 20:24:12 -0400

    Add save json task (#11)

--- src/io_collection/save/__init__.py
+++ src/io_collection/save/__init__.py
@@ -1,4 +1,5 @@
 from .save_buffer import save_buffer
 from .save_figure import save_figure
+from .save_json import save_json
 from .save_text import save_text
 from .save_dataframe import save_dataframe
