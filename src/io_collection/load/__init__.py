47f34a9e98e5ce065b96eb0ec3c0d16e6982fc2e
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-03-29 19:01:32 -0400

    Add task decorator to init imports

--- src/io_collection/load/__init__.py
+++ src/io_collection/load/__init__.py
@@ -1,6 +1,3 @@
-import importlib
-import sys
-
 from prefect import task
 
 from .load_buffer import load_buffer
@@ -10,16 +7,9 @@ from .load_pickle import load_pickle
 from .load_tar import load_tar
 from .load_text import load_text
 
-TASK_MODULES = [
-    load_buffer,
-    load_dataframe,
-    load_image,
-    load_pickle,
-    load_tar,
-    load_text,
-]
-
-for task_module in TASK_MODULES:
-    MODULE_NAME = task_module.__name__
-    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
-    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
+load_buffer = task(load_buffer)
+load_dataframe = task(load_dataframe)
+load_image = task(load_image)
+load_pickle = task(load_pickle)
+load_tar = task(load_tar)
+load_text = task(load_text)
