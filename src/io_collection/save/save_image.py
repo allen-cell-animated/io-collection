fcd4b89da6345ba87724bbf6eef70a343f20fc50
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-07-06 11:31:35 -0400

    Add option to save to png in save image task (#43)

--- src/io_collection/save/save_image.py
+++ src/io_collection/save/save_image.py
@@ -1,9 +1,13 @@
+import io
 import os
 import tempfile
 
 import boto3
 import numpy as np
 from aicsimageio.writers import OmeTiffWriter
+from PIL import Image
+
+from io_collection.save.save_buffer import save_buffer_to_s3
 
 
 def save_image(location: str, key: str, image: np.ndarray) -> None:
@@ -16,15 +20,24 @@ def save_image(location: str, key: str, image: np.ndarray) -> None:
 def save_image_to_fs(path: str, key: str, image: np.ndarray) -> None:
     full_path = os.path.join(path, key)
     os.makedirs(os.path.split(full_path)[0], exist_ok=True)
-    OmeTiffWriter.save(image, full_path)
+
+    if key.endswith(".ome.tiff") or key.endswith(".ome.tif"):
+        OmeTiffWriter.save(image, full_path)
+    elif key.endswith(".png"):
+        Image.fromarray(image).save(full_path)
 
 
 def save_image_to_s3(bucket: str, key: str, image: np.ndarray) -> None:
     s3_client = boto3.client("s3")
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_path = os.path.join(temp_dir, "temp.ome.tiff")
-        OmeTiffWriter.save(image, temp_path)
+    if key.endswith(".ome.tiff") or key.endswith(".ome.tif"):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = os.path.join(temp_dir, "temp.ome.tiff")
+            OmeTiffWriter.save(image, temp_path)
 
-        with open(temp_path, "rb") as fileobj:
-            s3_client.upload_fileobj(fileobj, bucket, key)
+            with open(temp_path, "rb") as fileobj:
+                s3_client.upload_fileobj(fileobj, bucket, key)
+    elif key.endswith(".png"):
+        with io.BytesIO() as buffer:
+            Image.fromarray(image).save(buffer, format="png")
+            save_buffer_to_s3(bucket, key, buffer)
