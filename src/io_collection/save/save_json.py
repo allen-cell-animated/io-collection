fba5c6598176deee227abfcca668f9fd0f7684ed
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-07-13 20:33:01 -0400

    Update save json format pattern for negatives and exponents (#44)

--- src/io_collection/save/save_json.py
+++ src/io_collection/save/save_json.py
@@ -9,7 +9,7 @@ def save_json(location: str, key: str, obj: Union[dict, list], levels: int = 5) 
     contents = json.dumps(obj, indent=2)
 
     for level in range(1, levels + 1):
-        match_pattern = r"\[" + ",".join([r"\n\s*([\d\.]+)" for i in range(level)]) + r"\n\s*\]"
+        match_pattern = r"\[" + ",".join([r"\n\s*([e\-\d\.]+)" for i in range(level)]) + r"\n\s*\]"
         sub_pattern = "[" + ",".join([f"\\{i + 1}" for i in range(level)]) + "]"
         pattern = re.compile(match_pattern)
         contents = pattern.sub(sub_pattern, contents)
