9f6a52eea29299df41471cffa2c53ed9f3edb391
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-05-06 15:52:30 -0400

    Update load image task to use bioio

--- src/io_collection/load/load_image.py
+++ src/io_collection/load/load_image.py
@@ -1,30 +1,31 @@
 import os
 from typing import Optional
 
-from aicsimageio import AICSImage
-from aicsimageio.readers import OmeTiffReader, TiffReader
+import bioio_ome_tiff
+import bioio_tifffile
+from bioio import BioImage
 
 
-def load_image(location: str, key: str, dim_order: Optional[str] = None) -> AICSImage:
+def load_image(location: str, key: str, dim_order: Optional[str] = None) -> BioImage:
     if location[:5] == "s3://":
         return load_image_from_s3(location[5:], key, dim_order)
     else:
         return load_image_from_fs(location, key, dim_order)
 
 
-def load_image_from_fs(path: str, key: str, dim_order: Optional[str] = None) -> AICSImage:
+def load_image_from_fs(path: str, key: str, dim_order: Optional[str] = None) -> BioImage:
     full_path = os.path.join(path, key)
-    return AICSImage(
+    return BioImage(
         full_path,
-        reader=OmeTiffReader if key.endswith(".ome.tiff") else TiffReader,
+        reader=bioio_ome_tiff.Reader if ".ome.tif" in key else bioio_tifffile.Reader,
         dim_order=dim_order,
     )
 
 
-def load_image_from_s3(bucket: str, key: str, dim_order: Optional[str] = None) -> AICSImage:
+def load_image_from_s3(bucket: str, key: str, dim_order: Optional[str] = None) -> BioImage:
     full_key = f"s3://{bucket}/{key}"
-    return AICSImage(
+    return BioImage(
         full_key,
-        reader=OmeTiffReader if key.endswith(".ome.tiff") else TiffReader,
+        reader=bioio_ome_tiff.Reader if ".ome.tif" in key else bioio_tifffile.Reader,
         dim_order=dim_order,
     )
