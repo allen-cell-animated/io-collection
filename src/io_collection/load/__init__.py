2feeddd13b04208a23fb6213f14c05768ffae722
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-06-07 16:09:17 -0400

    Update image task imports for optional dependencies

--- src/io_collection/load/__init__.py
+++ src/io_collection/load/__init__.py
@@ -4,7 +4,6 @@ from prefect import task
 
 from .load_buffer import load_buffer
 from .load_dataframe import load_dataframe
-from .load_image import load_image
 from .load_json import load_json
 from .load_pickle import load_pickle
 from .load_tar import load_tar
@@ -12,8 +11,13 @@ from .load_text import load_text
 
 load_buffer = task(load_buffer)
 load_dataframe = task(load_dataframe)
-load_image = task(load_image)
 load_json = task(load_json)
 load_pickle = task(load_pickle)
 load_tar = task(load_tar)
 load_text = task(load_text)
+
+try:
+    from .load_image import load_image
+    load_image = task(load_image)
+except ImportError:
+    pass
