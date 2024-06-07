d6cee3b0cae301c24d74ee862e43e03a5b1749c4
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2024-06-07 16:32:59 -0400

    Separate image dependencies to optional group (#56)
    * Move bioio to optional dependencies
    
    * Update image task imports for optional dependencies
    
    * Update installtion instructions for optional images dependencies
    
    * Fix linting
    
    * Update workflows to install extras
    
    * Update lint workflow to install library
    
    * Move all extras to dependencies step

--- src/io_collection/save/__init__.py
+++ src/io_collection/save/__init__.py
@@ -6,7 +6,6 @@ from .save_buffer import save_buffer
 from .save_dataframe import save_dataframe
 from .save_figure import save_figure
 from .save_gif import save_gif
-from .save_image import save_image
 from .save_json import save_json
 from .save_pickle import save_pickle
 from .save_tar import save_tar
@@ -16,8 +15,14 @@ save_buffer = task(save_buffer)
 save_dataframe = task(save_dataframe)
 save_figure = task(save_figure)
 save_gif = task(save_gif)
-save_image = task(save_image)
 save_json = task(save_json)
 save_pickle = task(save_pickle)
 save_tar = task(save_tar)
 save_text = task(save_text)
+
+try:
+    from .save_image import save_image
+
+    save_image = task(save_image)
+except ImportError:
+    pass
