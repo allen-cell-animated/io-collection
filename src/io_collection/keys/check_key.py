129efb2b17f08a1b55cf694e0d89258193d45197
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-26 17:39:57 -0400

    Add docstring and unit tests for check key task

--- src/io_collection/keys/check_key.py
+++ src/io_collection/keys/check_key.py
@@ -4,18 +4,68 @@ import boto3
 
 
 def check_key(location: str, key: str) -> bool:
+    """
+    Check if object key exists at specified location.
+
+    Method will check in the S3 bucket if the location begins with the **s3://**
+    protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    key
+        Object key.
+
+    Returns
+    -------
+    :
+        True if the object exists in the location, False otherwise.
+    """
+
     if location[:5] == "s3://":
         return check_key_on_s3(location[5:], key)
-    else:
-        return check_key_on_fs(location, key)
+    return check_key_on_fs(location, key)
 
 
 def check_key_on_fs(path: str, key: str) -> bool:
+    """
+    Check if object key exists on local file system.
+
+    Parameters
+    ----------
+    path
+        Local object path.
+    key
+        Object key.
+
+    Returns
+    -------
+    bool
+        True if the object exists on the local file system, False otherwise.
+    """
+
     full_path = os.path.join(path, key)
     return os.path.isfile(full_path)
 
 
 def check_key_on_s3(bucket: str, key: str) -> bool:
+    """
+    Check if object key exists in AWS S3 bucket.
+
+    Parameters
+    ----------
+    path
+        AWS S3 bucket name.
+    key
+        Object key.
+
+    Returns
+    -------
+    bool
+        True if the object exists in the AWS bucket, False otherwise.
+    """
+
     s3_client = boto3.client("s3")
     result = s3_client.list_objects(Bucket=bucket, Prefix=key)
     return "Contents" in result
