b19c7fddc822a8d02b0906cf2daa58ddff2c2273
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-26 18:10:06 -0400

    Add docstring and unit tests for get keys task

--- src/io_collection/keys/get_keys.py
+++ src/io_collection/keys/get_keys.py
@@ -5,13 +5,47 @@ import boto3
 
 
 def get_keys(location: str, prefix: str) -> list[str]:
+    """
+    Get list of objects at specified location with given prefix.
+
+    Method will check in the S3 bucket if the location begins with the **s3://**
+    protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys at location.
+    """
+
     if location[:5] == "s3://":
         return get_keys_from_s3(location[5:], prefix)
-    else:
-        return get_keys_from_fs(location, prefix)
+    return get_keys_from_fs(location, prefix)
 
 
 def get_keys_from_fs(path: str, prefix: str) -> list[str]:
+    """
+    Get list of objects on local file system with given prefix.
+
+    Parameters
+    ----------
+    location
+        Local object path.
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys on the local file system.
+    """
+
     glob_pattern = f"{path}/{prefix}/**/*".replace("//", "/")
     all_files = glob(glob_pattern, recursive=True)
     regular_files = [file for file in all_files if not os.path.isdir(file)]
@@ -20,6 +54,22 @@ def get_keys_from_fs(path: str, prefix: str) -> list[str]:
 
 
 def get_keys_from_s3(bucket: str, prefix: str) -> list[str]:
+    """
+    Get list of objects in AWS S3 bucket with given prefix.
+
+    Parameters
+    ----------
+    location
+        AWS S3 bucket name.
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys in the AWS bucket.
+    """
+
     s3_client = boto3.client("s3")
 
     bucket = bucket.replace("s3://", "")
