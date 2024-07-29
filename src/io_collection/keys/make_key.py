5893b4c079041f897d2547142bc58aca1a6a5d59
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-29 11:45:33 -0400

    Add docstring and unit tests for make key task

--- src/io_collection/keys/make_key.py
+++ src/io_collection/keys/make_key.py
@@ -3,6 +3,21 @@ from datetime import datetime
 
 
 def make_key(*subkeys: str) -> str:
+    """
+    Combines given subkeys into a single key.
+
+    If any subkeys include **{{timestamp}}**, it will be replaced with the
+    current date formatted as **YYYY-MM-DD**. Any instances of double
+    underscores (`__`) are replaced with a single underscore (`_`). Any
+    instances of underscore followed by a period (`_.`) are replaced with a
+    period (`.`).
+
+    Returns
+    -------
+    :
+        The key.
+    """
+
     key = os.path.join(*subkeys)
 
     timestamp = datetime.now().strftime("%Y-%m-%d")
---
+++ tests/io_collection/keys/test_make_key.py
@@ -0,0 +1,43 @@
+import datetime
+import random
+import sys
+import unittest
+from unittest import mock
+
+from io_collection.keys.make_key import make_key
+
+
+class TestMakeKey(unittest.TestCase):
+    def test_make_key(self):
+        subkeys = ["aaa", "bbb", "ccc.ext"]
+        expected_key = "aaa/bbb/ccc.ext"
+        key = make_key(*subkeys)
+        self.assertEqual(expected_key, key)
+
+    def test_make_key_double_underscore(self):
+        subkeys = ["aaa", "bb__b", "ccc.ext"]
+        expected_key = "aaa/bb_b/ccc.ext"
+        key = make_key(*subkeys)
+        self.assertEqual(expected_key, key)
+
+    def test_make_key_underscore_extension(self):
+        subkeys = ["aaa", "bbb", "ccc_.ext"]
+        expected_key = "aaa/bbb/ccc.ext"
+        key = make_key(*subkeys)
+        self.assertEqual(expected_key, key)
+
+    @mock.patch.object(sys.modules["io_collection.keys.make_key"], "datetime")
+    def test_make_key_with_timestamp(self, datetime_mock):
+        year = random.randint(2000, 2050)
+        month = random.randint(1, 12)
+        day = random.randint(1, 28)
+        datetime_mock.now.return_value = datetime.date(year, month, day)
+
+        subkeys = ["aaa", "{{timestamp}}", "ccc.ext"]
+        expected_key = f"aaa/{year}-{month:02d}-{day:02d}/ccc.ext"
+        key = make_key(*subkeys)
+        self.assertEqual(expected_key, key)
+
+
+if __name__ == "__main__":
+    unittest.main()
