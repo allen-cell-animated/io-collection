5f5c29afe3134f83afda4284c7c583e9063ca71a
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-29 17:03:44 -0400

    Add docstrings and unit tests for load image task

--- src/io_collection/load/load_image.py
+++ src/io_collection/load/load_image.py
@@ -5,15 +5,56 @@ import bioio_ome_tiff
 import bioio_tifffile
 from bioio import BioImage
 
+EXTENSIONS = (".tif", ".tiff", ".ome.tif", ".ome.tiff")
+
 
 def load_image(location: str, key: str, dim_order: Optional[str] = None) -> BioImage:
+    """
+    Load key as BioIO image from specified location.
+
+    Method will load from the S3 bucket if the location begins with the
+    **s3://** protocol, otherwise it assumes the location is a local path.
+
+    Method currently only supports `.tiff` and `ome.tiff` images.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    key
+        Object key ending in `.tiff` or `ome.tiff`.
+
+    Returns
+    -------
+    :
+        Loaded image.
+    """
+
+    if not key.endswith(EXTENSIONS):
+        raise ValueError(f"key [ {key} ] must have [ {' | '.join(EXTENSIONS)} ] extension")
+
     if location[:5] == "s3://":
         return load_image_from_s3(location[5:], key, dim_order)
-    else:
-        return load_image_from_fs(location, key, dim_order)
+    return load_image_from_fs(location, key, dim_order)
 
 
 def load_image_from_fs(path: str, key: str, dim_order: Optional[str] = None) -> BioImage:
+    """
+    Load key as BioIO image from local file system.
+
+    Parameters
+    ----------
+    path
+        Local object path.
+    key
+        Object key ending in `.tiff` or `ome.tiff`.
+
+    Returns
+    -------
+    :
+        Loaded image.
+    """
+
     full_path = os.path.join(path, key)
     return BioImage(
         full_path,
@@ -23,6 +64,22 @@ def load_image_from_fs(path: str, key: str, dim_order: Optional[str] = None) -> 
 
 
 def load_image_from_s3(bucket: str, key: str, dim_order: Optional[str] = None) -> BioImage:
+    """
+    Load key as BioIO image from AWS S3 bucket.
+
+    Parameters
+    ----------
+    bucket
+        AWS S3 bucket name.
+    key
+        Object key ending in `.tiff` or `ome.tiff`.
+
+    Returns
+    -------
+    :
+        Loaded image.
+    """
+
     full_key = f"s3://{bucket}/{key}"
     return BioImage(
         full_key,
---
+++ tests/io_collection/load/test_load_image.py
@@ -0,0 +1,62 @@
+import sys
+import unittest
+from unittest import mock
+
+import bioio_ome_tiff
+import bioio_tifffile
+
+from io_collection.load.load_image import load_image
+
+
+class TestLoadImage(unittest.TestCase):
+    def test_load_image_invalid_extension_throws_exception(self):
+        with self.assertRaises(ValueError):
+            load_image("", "key.ext")
+
+    @mock.patch.object(sys.modules["io_collection.load.load_image"], "BioImage")
+    def test_load_image_from_fs_tiff(self, bioimage_mock):
+        path = "test/path"
+        key = "key.tif"
+        dim_order = "ABC"
+
+        _ = load_image(path, key, dim_order=dim_order)
+        bioimage_mock.assert_called_with(
+            f"{path}/{key}", reader=bioio_tifffile.Reader, dim_order=dim_order
+        )
+
+    @mock.patch.object(sys.modules["io_collection.load.load_image"], "BioImage")
+    def test_load_image_from_fs_ome_tiff(self, bioimage_mock):
+        path = "test/path"
+        key = "key.ome.tif"
+        dim_order = "ABC"
+
+        _ = load_image(path, key, dim_order=dim_order)
+        bioimage_mock.assert_called_with(
+            f"{path}/{key}", reader=bioio_ome_tiff.Reader, dim_order=dim_order
+        )
+
+    @mock.patch.object(sys.modules["io_collection.load.load_image"], "BioImage")
+    def test_load_image_from_s3_tiff(self, bioimage_mock):
+        bucket = "test-bucket"
+        key = "key.tif"
+        dim_order = "ABC"
+
+        _ = load_image(f"s3://{bucket}", key, dim_order=dim_order)
+        bioimage_mock.assert_called_with(
+            f"s3://{bucket}/{key}", reader=bioio_tifffile.Reader, dim_order=dim_order
+        )
+
+    @mock.patch.object(sys.modules["io_collection.load.load_image"], "BioImage")
+    def test_load_image_from_s3_ome_tiff(self, bioimage_mock):
+        bucket = "test-bucket"
+        key = "key.ome.tif"
+        dim_order = "ABC"
+
+        _ = load_image(f"s3://{bucket}", key, dim_order=dim_order)
+        bioimage_mock.assert_called_with(
+            f"s3://{bucket}/{key}", reader=bioio_ome_tiff.Reader, dim_order=dim_order
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
