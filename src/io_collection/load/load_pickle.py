640cfd764526cc8f18efa84a840fed345dd840f9
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-29 15:49:22 -0400

    Add docstrings and unit tests for load pickle task

--- src/io_collection/load/load_pickle.py
+++ src/io_collection/load/load_pickle.py
@@ -5,5 +5,27 @@ from io_collection.load.load_buffer import load_buffer
 
 
 def load_pickle(location: str, key: str) -> Any:
+    """
+    Load key as pickled object from specified location.
+
+    Method will load from the S3 bucket if the location begins with the
+    **s3://** protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    key
+        Object key ending in `.pkl`.
+
+    Returns
+    -------
+    :
+        Loaded object.
+    """
+
+    if not key.endswith(".pkl"):
+        raise ValueError(f"key [ {key} ] must have [ pkl ] extension")
+
     buffer = load_buffer(location, key)
     return pickle.loads(buffer.read())
---
+++ tests/io_collection/load/test_load_pickle.py
@@ -0,0 +1,53 @@
+import pickle
+import random
+import string
+import unittest
+
+import boto3
+from moto import mock_aws
+from pyfakefs.fake_filesystem_unittest import patchfs
+
+from io_collection.load.load_pickle import load_pickle
+
+
+class TestLoadPickle(unittest.TestCase):
+    def setUp(self) -> None:
+        self.object = {
+            "ints": [random.randint(0, 100) for _ in range(100)],
+            "floats": [random.random() for _ in range(100)],
+            "strings": [random.choice(string.ascii_letters) for _ in range(100)],
+        }
+
+    def test_load_pickle_invalid_extension_throws_exception(self):
+        with self.assertRaises(ValueError):
+            load_pickle("", "key.ext")
+
+    @patchfs
+    def test_load_pickle_from_fs(self, fs):
+        path = "test/path"
+        key = "key.pkl"
+        contents = pickle.dumps(self.object)
+
+        fs.create_file(f"{path}/{key}", contents=contents)
+
+        pickle_object = load_pickle(path, key)
+
+        self.assertEqual(self.object, pickle_object)
+
+    @mock_aws
+    def test_load_pickle_from_s3(self):
+        bucket = "test-bucket"
+        key = "key.pkl"
+        contents = pickle.dumps(self.object)
+
+        s3_client = boto3.client("s3", region_name="us-east-1")
+        s3_client.create_bucket(Bucket=bucket)
+        s3_client.put_object(Bucket=bucket, Key=key, Body=contents)
+
+        pickle_object = load_pickle(f"s3://{bucket}", key)
+
+        self.assertEqual(self.object, pickle_object)
+
+
+if __name__ == "__main__":
+    unittest.main()
