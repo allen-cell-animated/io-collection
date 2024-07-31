c3748b8c1da19da5bc92a25aa876ae782a6a905b
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-31 12:15:39 -0400

    Add docstrings and unit tests for save pickle task

--- src/io_collection/save/save_pickle.py
+++ src/io_collection/save/save_pickle.py
@@ -6,6 +6,25 @@ from io_collection.save.save_buffer import save_buffer
 
 
 def save_pickle(location: str, key: str, obj: Any) -> None:
+    """
+    Save pickled object to key at specified location.
+
+    Method will save to the S3 bucket if the location begins with the **s3://**
+    protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    key
+        Object key ending in `.pkl`.
+    obj
+        Object to save.
+    """
+
+    if not key.endswith(".pkl"):
+        raise ValueError(f"key [ {key} ] must have [ pkl ] extension")
+
     with io.BytesIO() as buffer:
         pickle.dump(obj, buffer)
         save_buffer(location, key, buffer)
