2bb24dedbb0529e958ae5dcc46fd5e2a0aeb1c1c
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-08-01 17:37:09 -0400

    Add docstrings and unit test for load quilt package task

--- src/io_collection/quilt/load_quilt_package.py
+++ src/io_collection/quilt/load_quilt_package.py
@@ -2,5 +2,23 @@ import quilt3
 
 
 def load_quilt_package(quilt_package: str, quilt_registry: str) -> quilt3.Package:
+    """
+    Load package into memory from registry.
+
+    Package is loaded without making a local copy of the manifest.
+
+    Parameters
+    ----------
+    quilt_package
+        Quilt package name, formatted as {namespace}/{packagename}.
+    quilt_registry
+        Package registry name.
+
+    Returns
+    -------
+    :
+        The Quilt package.
+    """
+
     package = quilt3.Package.browse(quilt_package, quilt_registry)
     return package
